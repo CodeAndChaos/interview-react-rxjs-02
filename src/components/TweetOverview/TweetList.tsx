d57ce334e1be165225a9e5587cb36eb4c4671a3d
Neal <info@nealbuerger.com>
2021-02-04 16:04:10 +0100

    add fav count

--- src/components/TweetOverview/TweetList.tsx
+++ src/components/TweetOverview/TweetList.tsx
@@ -1,7 +1,14 @@
 import React, { useLayoutEffect, useState } from "react";
+import styled from "styled-components";
 import { initialState, twitterStore } from "../../data/state";
 import { TweetDisplayFilters } from "../TweetOverview";
 import { Tweet } from "./TweetDisplay";
+
+const FavCount = styled.div`
+  display: flex;
+  margin-top: 1rem;
+
+`
 
 export const DisplayTweets: React.FC<{
   filter: TweetDisplayFilters;
@@ -16,15 +23,18 @@ export const DisplayTweets: React.FC<{
     Recent: () => items.tweets,
     All: () => [...items.tweets, ...items.archive],
     Favorites: () =>
-      [...items.tweets, ...items.archive].filter(
-        (val) => val.favorite === true
-      ),
+      [...items.tweets, ...items.archive].filter((val) => val.favorite),
   };
   const tweetArray = filterMap[filter]();
 
   const displayTweets = [
     ...tweetArray.map((val) => <Tweet key={val.id} tweet={val} />),
   ];
-
-  return <>{displayTweets}</>;
+  const favoriteCount = tweetArray.filter((val) => val.favorite).length;
+  return (
+    <>
+      <FavCount>Favorite Count: {favoriteCount}</FavCount>
+      {displayTweets}
+    </>
+  );
 };
